bd336c126bf736acfbc871ee5452219cf0892fee
myrmik <myrmik@gmail.com>
2022-04-10 17:53:41 +0300

    some comment, UPD VS to 2022

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,39 @@
 //#include <PCH.hpp>
 #include "core/Game.hpp"
-
 #include "scenes/GameScene.hpp"
-
 
 #include <memory>
 
-//#include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+
+namespace NDefaultParam
+{
+	const sf::VideoMode mode = sf::VideoMode(1366u, 768u);
+	//const sf::VideoMode mode = sf::VideoMode( 1366u, 768u );
+	//const sf::VideoMode mode = sf::VideoMode::getDesktopMode();
+	//const sf::VideoMode mode = sf::VideoMode(*1024u, 768u);
+
+	constexpr char* title = "Roguelike Template";
+	constexpr sf::Uint32 style = sf::Style::Default /*sf::Style::Close | sf::Style::Titlebar | sf::Style::Resize*/;
+
+	constexpr unsigned int FPS = 60;
+}
+
 
 // Entry point of the application.
 int main()
 {
 	/**  todo
-	* 1 avoid creation windows directly - push into game
+	* 1 avoid creation windows directly - push into game, 
+	    OR provide read param class and read param from disk
 	* 2 get rid sf:: refers - provide adapter
 	* 4 base scene or game is resurse managed
 	* */
 
 
-
-
 	// Create the main game object.
-	//sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Roguelike Template", sf::Style::Fullscreen);
-	sf::RenderWindow window(sf::VideoMode(1366u, 768u/*1024u, 768u*/), "Roguelike Template", sf::Style::Default /*sf::Style::Close | sf::Style::Titlebar | sf::Style::Resize*/);
-	Game game(&window);
+	sf::RenderWindow window(NDefaultParam::mode, NDefaultParam::title, NDefaultParam::style);
+	Game game(&window, NDefaultParam::FPS);
 
 	game.setScene(std::make_shared<GameScene>(game));
 
